7dd5a93e3805690061a634e21dcf885938924ecc
Rodrigo Fernandes <rtfrodrigo@gmail.com>
2013-06-05 20:13:54 +0100

    Add proper HTTP/POST implementation

--- qamine-challenge/qamine.scala
+++ qamine-challenge/qamine.scala
@@ -2,15 +2,15 @@
 // Qamine Engineering Team Challenge
 // 
 // Author: Rodrigo Fernandes
-// Create: 04-06-2013 09:18
+// Create: 05-06-2013 20:12
 
 
 import scala.io.Source.fromInputStream
-import java.net.{URLConnection, URL}
-import sys.process._
+import java.net.{HttpURLConnection, URL}
+import java.io.{OutputStreamWriter}
 
-object QamineChallange {
-    var contact = "teste@gmail.com"
+object Qamine {
+    var contact = "rodrigo.fernandes@ist.utl.pt"
 
     def add(m: Int, n: Int) = m + n
     def subtract(m: Int, n: Int) = n - m
@@ -47,15 +47,25 @@ object QamineChallange {
         println("[ID:" + numbers(2) + "] " + operation(0) + " " + numbers(0) + " " + numbers(1) + " = " + payload)
         
         // Prepare HTTP/POST adress and arguments
-        var urlDest = new URL("http://powerful-fortress-5090.herokuapp.com/answer")
+        var answerURL = new URL("http://powerful-fortress-5090.herokuapp.com/answer")
         var args = "payload=" + payload + "&contact=" + contact + "&id=" + numbers(2)
         
-        // VERY ugly way of doing HTTP/POST could not use any of java stuff
-        // In java.net.URLConnection method to set POST is missing
-        // In org.apache classes to use HTTP/POST missing could not import them into the program
-        // Needs fix
-        var command = "curl -q --data " + args + " http://powerful-fortress-5090.herokuapp.com/answer"
-        command.!
+        // Get connection to answer
+        var answerConnection = answerURL.openConnection().asInstanceOf[HttpURLConnection]
+        answerConnection.setRequestMethod("POST")
+        answerConnection.setDoOutput(true)
+        
+        // Send answer
+        var wr = new OutputStreamWriter(answerConnection.getOutputStream())
+		wr.write(args);
+		wr.flush();
+		wr.close();
+		
+		// Print the answer
+        val answerLines = fromInputStream(answerConnection.getInputStream()).getLines
+        while(answerLines.hasNext) {
+        	println(answerLines.next)
+        }
     }
 }
 
