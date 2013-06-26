c8ebfcf2a657aac3f6e110f20ce2199a74fe83fd
Rodrigo Fernandes <rtfrodrigo@gmail.com>
2013-06-26 20:53:10 +0100

    Refactor code & Replace all var by val & Clean code

--- qamine-challenge/qamine.scala
+++ qamine-challenge/qamine.scala
@@ -3,69 +3,67 @@
 // 
 // Author: Rodrigo Fernandes
 // Create: 05-06-2013 20:12
-
+// Modified: 26-06-2013 20:19
 
 import scala.io.Source.fromInputStream
 import java.net.{HttpURLConnection, URL}
 import java.io.{OutputStreamWriter}
 
 object Qamine {
-    var contact = "rodrigo.fernandes@ist.utl.pt"
-
-    def add(m: Int, n: Int) = m + n
-    def subtract(m: Int, n: Int) = n - m
-    def multiply(m: Int, n: Int) = m * n
-    def divide(m: Int, n: Int) = m / n
-
-    def matchOperation(x: String, m: Int, n: Int): Int = x match {
-        case "add" => add(m, n)
-        case "subtract" => subtract(m, n)
-        case "multiply" => multiply(m, n)
-        case "divide" => divide(m, n)
-    } 
+    val contact = "rodrigo.fernandes@ist.utl.pt"
+      
+    def matchOperation(x: String, m: Int, n: Int) = x match {
+        case "add" => m + n
+        case "subtract" => n - m
+        case "multiply" => m * n
+        case "divide" => m / n
+    }
+    
+    def sendHTTPPOSTMessage(url: String, args: String) = {
+		val answerURL = new URL(url)
+		
+		// Setup connection to answer the challenge
+		val answerConnection = answerURL.openConnection().asInstanceOf[HttpURLConnection]
+		answerConnection.setRequestMethod("POST")
+		answerConnection.setDoOutput(true)
+		
+		// Send answer to the challenge
+		val wr = new OutputStreamWriter(answerConnection.getOutputStream())
+		wr.write(args)
+		wr.flush()
+		wr.close()
+		fromInputStream(answerConnection.getInputStream()).mkString
+    }
 
     def main(args: Array[String]) {     
         // Get web page and extract challenge
         val challengeURL = new URL("http://powerful-fortress-5090.herokuapp.com/challenge")
-        val challengeConnection = challengeURL.openConnection()
-        val pageLines = fromInputStream(challengeConnection.getInputStream).getLines
-        var challenge = pageLines.next()
+        val challengeConnection = challengeURL.openConnection
+        val challenge = fromInputStream(challengeConnection.getInputStream).getLines().next
+        
+        // [Debug] Print the challenge information received
         println("[CHALLENGE: " + challenge + "]")
         
-        // Define the define the operation and number regular expressions
-        val Operation = "(add|subtract|multiply|divide)".r
-        val Numbers = "([0-9]+)".r
+        // Define the operations and numbers regular expressions
+        val OperationsExpr = "(add|subtract|multiply|divide)".r
+        val NumbersExpr = "([0-9]+)".r
         
         // Get the operation and the numbers
-        val operation = Operation.findAllIn(challenge).toList
-        val numbers = Numbers.findAllIn(challenge).toList
-
+        val operation = OperationsExpr.findAllIn(challenge).next
+        val numbers = NumbersExpr.findAllIn(challenge).toList
+        
         // Calculate payload
-        var payload = matchOperation(operation(0), numbers(0).toInt, numbers(1).toInt)
+        val payload = matchOperation(operation, numbers(0).toInt, numbers(1).toInt)
 
-        // Debug print with all challenge info
-        println("[ID:" + numbers(2) + "] " + operation(0) + " " + numbers(0) + " " + numbers(1) + " = " + payload)
+        // [Debug] Print with all challenge info 
+        println("[ID:" + numbers(2) + "] " + operation + " " + numbers(0) + " " + numbers(1) + " = " + payload)
         
-        // Prepare HTTP/POST adress and arguments
-        var answerURL = new URL("http://powerful-fortress-5090.herokuapp.com/answer")
-        var args = "payload=" + payload + "&contact=" + contact + "&id=" + numbers(2)
+        // Prepare HTTP/POST address and arguments
+        val answerURL = "http://powerful-fortress-5090.herokuapp.com/answer"
+        val args = "payload=" + payload + "&contact=" + contact + "&id=" + numbers(2)
         
-        // Get connection to answer
-        var answerConnection = answerURL.openConnection().asInstanceOf[HttpURLConnection]
-        answerConnection.setRequestMethod("POST")
-        answerConnection.setDoOutput(true)
-        
-        // Send answer
-        var wr = new OutputStreamWriter(answerConnection.getOutputStream())
-		wr.write(args);
-		wr.flush();
-		wr.close();
-		
-		// Print the answer
-        val answerLines = fromInputStream(answerConnection.getInputStream()).getLines
-        while(answerLines.hasNext) {
-        	println(answerLines.next)
-        }
+        val answer = sendHTTPPOSTMessage(answerURL, args)
+        println(answer)
     }
 }
 
